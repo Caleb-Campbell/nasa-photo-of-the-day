db381ac239c6c46d94cd0e7a3cc8db9acfda163c
Caleb-Campbell <calebcampbellcrm@gmail.com>
2022-09-05 10:07:03 -0600

    Re-styled my page using styled components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import React, {useState, useEffect} from "react";
-import "./App.css";
 import axios from 'axios'
+import theme from "./theme";
+import styled from 'styled-components';
 
 //Imported Components
 import NasaPhoto from "./components/NasaPhoto";
 import Header from "./components/Header/Header";
 import Info from "./components/Info";
 
-// Used this varible 'dataDump' for building the app so there wouldn't be overloading
-// const dataDump = {
-//   copyright: "Wang Letian",
-//   date: "2022-09-03",
-//   explanation: "On August 25 Sun and Moon could both be seen in planet Earth's daytime skies. And so could the International Space Station. The ISS crossed the disk of the waning crescent Moon as seen from Shunyi district, Beijing, China at about 11:02 am local time. Some 40 kilometers to the southwest, in Fengtai district, the ISS was seen to cross the Sun's disk too. The solar transit was observed only 29 seconds later. Both transits are compared in these panels, composed of processed and stacked video frames from the two locations. The coordinated captures were made with different equipment, but adjusted to show the Sun and Moon at the same scale. The ISS was at a calculated range of 435 kilometers for the lunar transit and 491 kilometers when passing in front of the Sun.   Artemis I: Launch Update",
-//   hdurl: "https://apod.nasa.gov/apod/image/2209/Interval29seconds_Transit.jpg",
+// const dummyData = {
+//   date: "2022-09-05",
+//   explanation: "Stars created these cliffs. Specifically, the destructive winds and energetic light from the stars in the open cluster NGC 3324 eroded away part of a mountain of dark interstellar dust in the northern part of the Carina Nebula. Several of these stars are visible toward the top of this highly detailed image taken recently by the James Webb Space Telescope, the largest astronomical telescope ever launched. Webb's large mirror and ability to see dust-piercing infrared light has enabled it to capture fascinating details in the dust, hundreds of previously hidden stars, and even some galaxies far in the distance.  The featured jagged cliffs occur in part of Carina known as the Gabriela Mistral Nebula -- because when viewed in another orientation, they appear similar to the facial profile of the famous Chilean poet.  These nebular cliffs occur about 7,600 light years away toward the southern constellation of Carina.   Astrophysicists: Browse 2,800+ codes in the Astrophysics Source Code Library",
+//   hdurl: "https://apod.nasa.gov/apod/image/2209/CarinaCliffs_Webb_2000.jpg",
 //   media_type: "image",
 //   service_version: "v1",
-//   title: "Sun and Moon and ISS",
-//   url: "https://apod.nasa.gov/apod/image/2209/Interval29seconds_Transit1200.jpg"
+//   title: "Carina Cliffs from the Webb Space Telescope",
+//   url: "https://apod.nasa.gov/apod/image/2209/CarinaCliffs_Webb_1080.jpg"
 // }
 
+const StyledApp = styled.div`
+text-align: center;
+margin: 0 auto;
 
+`
 
 function App() {
   const [data, setData] = useState({})
@@ -27,17 +30,17 @@ function App() {
   useEffect(()=> {
     axios.get('https://api.nasa.gov/planetary/apod?api_key=iDawMAeL8rS7oZ3L6ZTXhNxX9Sk4tbmkj7NgqjfL')
     .then(res=> {
-  setData(res.data)
+      setData(res.data)
 },[])
 .catch(err => console.error(err))
   })
 
   return (
-    <div className="App">
-      <Header headerInfo={data} />
-      <NasaPhoto photo={data} />
-      <Info data={data} /> 
-    </div>
+    <StyledApp>
+      <Header headerInfo={data} theme={theme} />
+      <NasaPhoto photo={data} theme={theme} />
+      <Info data={data} theme={theme} /> 
+    </StyledApp>
   );
 }
 
